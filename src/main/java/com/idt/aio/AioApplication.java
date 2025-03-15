6257668697e76ea7a280b5326abda983ef8850b5
YoungKyonYou <man.spride1324@gmail.com>
2025-03-15 09:39:23 +0900

    chore: 프로파일링 추가

--- src/main/java/com/idt/aio/AioApplication.java
+++ src/main/java/com/idt/aio/AioApplication.java
@@ -2,7 +2,10 @@ package com.idt.aio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class AioApplication {
 
