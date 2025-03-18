3f3051c124201d92272eaa746b3ddd3b4829d7df
YoungKyonYou <man.spride1324@gmail.com>
2025-03-18 15:15:18 +0900

    refactor: table 관계 수정 및 삭제

--- src/main/java/com/idt/aio/repository/DocumentAttributeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.idt.aio.repository;
-
-import com.idt.aio.entity.DocumentAttribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DocumentAttributeRepository extends JpaRepository<DocumentAttribute, Integer> {
-}
