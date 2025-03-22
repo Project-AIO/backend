e9ebdd94bff0fbf825de4f53a2f36377aa2fd955
YoungKyonYou <man.spride1324@gmail.com>
2025-03-22 16:12:25 +0900

    fix: 오타 수정

--- src/main/java/com/idt/aio/entity/ConfigurationKnowledge.java
+++ src/main/java/com/idt/aio/entity/ConfigurationKnowledge.java
@@ -80,7 +80,7 @@ public class ConfigurationKnowledge {
                 .embModelName(embModelName)
                 .rerkModelName(rerkModelName)
                 .rerktopN(rerktopN)
-                .retvThresholdScore(retvThreshholdScore)
+                .retvThresholdScore(retvThresholdScore)
                 .retvTopK(retvTopK)
                 .keywordWeight(keywordWeight)
                 .build();
