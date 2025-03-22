aca639d0a62cec5c8f35e277e485f09bcf7e759d
김현우 <kyapt79@gmail.com>
2025-03-22 17:23:28 +0900

    fix: conflict

--- src/main/java/com/idt/aio/repository/AdminRepository.java
+++ src/main/java/com/idt/aio/repository/AdminRepository.java
@@ -1,9 +1,18 @@
 package com.idt.aio.repository;
 
 import com.idt.aio.entity.Admin;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, String> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Admin> findOneWithAuthoritiesByAdminId(String adminId);
+
+    Optional<Admin> findByAdminId(String adminId);
+
 }
